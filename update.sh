285d40afc1b61c6478f9f364bbc201617f17e0ff
Jordan Vani <jmv423@nyu.edu>
2021-01-04 21:02:46 -0700

    feat: revise auth

--- update.sh
+++ update.sh
@@ -8,9 +8,8 @@ UPDATED_MD5=$(md5sum jarchive.db)
 if [[ "$ORIGINAL_MD5" == "$UPDATED_MD5" ]]; then
   echo -e "Database unchanged \n\ta) $ORIGINAL_MD5\n\tb) $UPDATED_MD5"
 else
-  echo -e "Database updated \n\ta) $ORIGINAL_MD5\n\tb)$UPDATED_MD5"
-  git remote add origin https://jvani:$ACCESS_TOKEN@github.com/jvani/jarchive.git
+  echo -e "Database updated \n\ta) $ORIGINAL_MD5\n\tb) $UPDATED_MD5"
   git add jarchive.db
   git commit -m "feat: db update"
-  git push
+  git push https://$ACCESS_TOKEN@github.com/jvani/jarchive.git
 fi
